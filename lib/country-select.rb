6385a098e4797fe93840d749e3c58c4534767883
hiveage <hiveage@vesess.com>
2014-05-03 19:37:06 +0530

    changed "United States" --> "United States of America"

--- lib/country-select.rb
+++ lib/country-select.rb
@@ -69,7 +69,7 @@ module ActionView
         "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Timor-Leste",
         "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
         "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom",
-        "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu",
+        "United States of America", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu",
 				"Venezuela, Bolivarian Republic of", "Viet Nam", "Virgin Islands, British", "Virgin Islands, U.S.",
 				"Wallis and Futuna", "Western Sahara", "Yemen", "Zambia", "Zimbabwe"] unless const_defined?("COUNTRIES")
     end
